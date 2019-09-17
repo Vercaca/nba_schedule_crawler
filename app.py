f109c5a68e8a4e34e479790c51f2593b6f6b6703
veronica_cheang <veronica_cheang@gss.com.tw>
2019-09-17 10:46:33 +0800

    modified

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ from linebot.models import (
 )
 
 
-__LINE_CHANNEL_ACCESS_TOKEN__ = ''  # YOUR_CHANNEL_ACCESS_TOKEN
-__WEBHOOK_CHANNEL_SECRET__ = ''  # YOUR_CHANNEL_SECRET
+__LINE_CHANNEL_ACCESS_TOKEN__ = 'Rg7Ts917dOkKpnOqlGTklZrro3dOJ4BOuZro3zAajzp/' \
+                                'Z1O+h+dyTpQer6nYfTn1RMEuZ6ya/FpLx8fkvSgTRooY' \
+                                'KwD1rvVQ2oMj1m+fkW6gjv1HnxvZn7pFfMovLdPo1AiY' \
+                                'h4rI+kQqzUyHS6nlwwdB04t89/1O/w1cDnyilFU='  # YOUR_CHANNEL_ACCESS_TOKEN
+__WEBHOOK_CHANNEL_SECRET__ = 'fc8aa1a7bfe463c80b0f50de599eec94'  # YOUR_CHANNEL_SECRET
 
 app = Flask(__name__)
 
