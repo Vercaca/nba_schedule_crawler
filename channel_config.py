b133e43bf9d7d1f33173188f72afecc640f82825
Vercaca <verca2510@iir.csie.ncku.edu.tw>
2019-09-21 22:03:22 +0800

    fix channel config

--- channel_config.py
+++
@@ -1,4 +0,0 @@
-channel_config = {
-    'ACCESS_TOKEN': 'mF7WyHTQdEpMQhgFln6+klYO5hrrKngIIg8cl5G8uPHg4b2Rd0tlnH/4ObLk9mSXRMEuZ6ya/FpLx8fkvSgTRooYKwD1rvVQ2oMj1m+fkW52Tno9N1KdWMtWMPaSbi+2bNzTV0SUbP4axaUQyQc0CgdB04t89/1O/w1cDnyilFU=',
-    'SECRET': 'fc8aa1a7bfe463c80b0f50de599eec94'
-}
